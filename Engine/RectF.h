12b47e41dc71b743c8ca0d89c7d0b5f4ad8dfa7c
Sam Campbell <sam@sclighting.co.uk>
2020-01-11 01:10:32 +0000

    Added chili rect code for testing

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -6,9 +6,9 @@ class RectF
 public:
 	RectF() = default;
 	RectF(const float in_top, float in_bottom, float in_left, float in_right);
-	RectF(const Vec2& topLeft_in, Vec2& bottomRight_in);
+	RectF(const Vec2& topLeft,const Vec2& bottomRight);
 	RectF(const Vec2& topLeft_in, float width_in, float height_in);
-	bool OverlapTest(RectF& incoming_rect);
+	bool OverlapTest(const RectF& other);
 	static RectF FromCenter(const Vec2& center, float offset_x, float offset_y);
 
 public:
